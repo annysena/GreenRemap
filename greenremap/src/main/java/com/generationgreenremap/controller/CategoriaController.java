ec53c10876ce2bfdbe90e3ae37d7eca574ca4404
basanntana <barbarasantanabr@gmail.com>
2021-08-03 11:07:52 -0300

    Metodo Get Id

--- greenremap/src/main/java/com/generationgreenremap/controller/CategoriaController.java
+++ greenremap/src/main/java/com/generationgreenremap/controller/CategoriaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +27,15 @@ public class CategoriaController {
 		return ResponseEntity.ok(categoriaRepository.findAll());
 		
 	}
-
+  
+	@GetMapping("/{Id}")
+	public ResponseEntity<Categoria> getById(@PathVariable long id){
+		
+		return categoriaRepository.findById(id)
+				.map(resp->ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+				
+		
+	}
+	
 }
