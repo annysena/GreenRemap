260a07c9b227df221e97bb5bc5221e4251999ccf
Guilherme (Gillie) <miscildes@gmail.com>
2021-08-03 11:53:10 -0300

    metodo PUT e Delete

--- greenremap/src/main/java/com/generationgreenremap/controller/CategoriaController.java
+++ greenremap/src/main/java/com/generationgreenremap/controller/CategoriaController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,5 +49,15 @@ public class CategoriaController {
 	@PostMapping
 	public ResponseEntity<Categoria> postCategoria(@RequestBody Categoria categoria) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaRepository.save(categoria));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Categoria> putCategoria(@RequestBody Categoria categoria) {
+		return ResponseEntity.status(HttpStatus.OK).body(categoriaRepository.save(categoria));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteCategoria(@PathVariable long id) {
+		categoriaRepository.deleteById(id);
 	}
 }
