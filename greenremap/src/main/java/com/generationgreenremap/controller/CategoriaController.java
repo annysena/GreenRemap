b00f5e85926ed6240d21683ee351dcca84428ac5
annysena <annysena.as44@gmail.com>
2021-08-03 11:30:23 -0300

    Adicionando getByPalavrachave e PostMapping

--- greenremap/src/main/java/com/generationgreenremap/controller/CategoriaController.java
+++ greenremap/src/main/java/com/generationgreenremap/controller/CategoriaController.java
@@ -3,10 +3,13 @@ package com.generationgreenremap.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,25 +20,32 @@ import com.generationgreenremap.repository.CategoriaRepository;
 @RequestMapping("/categoria")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CategoriaController {
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@GetMapping
 	public ResponseEntity<List<Categoria>> getAll() {
-		
+
 		return ResponseEntity.ok(categoriaRepository.findAll());
-		
+
 	}
-  
+
 	@GetMapping("/{Id}")
-	public ResponseEntity<Categoria> getById(@PathVariable long id){
-		
-		return categoriaRepository.findById(id)
-				.map(resp->ResponseEntity.ok(resp))
+	public ResponseEntity<Categoria> getById(@PathVariable long id) {
+
+		return categoriaRepository.findById(id).map(resp -> ResponseEntity.ok(resp))
 				.orElse(ResponseEntity.notFound().build());
-				
-		
 	}
-	
+
+	@GetMapping("/palavrachave/{palavrachave}")
+	public ResponseEntity<List<Categoria>> getByPalavraChave(@PathVariable String palavrachave) {
+		return ResponseEntity.ok(categoriaRepository.findAllByPalavrachaveContainingIgnoreCase(palavrachave));
+
+	}
+
+	@PostMapping
+	public ResponseEntity<Categoria> postCategoria(@RequestBody Categoria categoria) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaRepository.save(categoria));
+	}
 }
