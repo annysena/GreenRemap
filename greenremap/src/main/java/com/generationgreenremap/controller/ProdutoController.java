48b2dabdf5ee3724a265ba40b90e475c36fae55f
annysena <annysena.as44@gmail.com>
2021-08-04 11:12:39 -0300

    Método GetByid do Produto

--- greenremap/src/main/java/com/generationgreenremap/controller/ProdutoController.java
+++ greenremap/src/main/java/com/generationgreenremap/controller/ProdutoController.java
@@ -6,19 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generationgreenremap.model.Produto;
 import com.generationgreenremap.repository.ProdutoRepository;
 
-
 @RestController
 @RequestMapping("/produto")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ProdutoController {
-	
-	
+
 	@Autowired
 	private ProdutoRepository produtoRepository;
 
@@ -26,8 +25,17 @@ public class ProdutoController {
 	public ResponseEntity<List<Produto>> getAll() {
 
 		return ResponseEntity.ok(produtoRepository.findAll());
-		
+	}
 
+	@GetMapping("/{id}")
+	private ResponseEntity<Produto> getById(@PathVariable long id) {
+		return produtoRepository.findById(id).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+
+	@GetMapping("/nomeproduto/{nomeproduto}")
+	public ResponseEntity<List<Produto>> GetBynomeproduto(@PathVariable String nomeproduto) {
+		return ResponseEntity.ok(produtoRepository.findAllByNomeprodutoContainingIgnoreCase(nomeproduto));
 	}
 
 }
